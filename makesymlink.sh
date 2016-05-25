302a49805bbb161910ca68025bd56cbc890da5d8
balint <balint@SpanishInquisition>
2016-05-25 11:32:44 +0100

    google now conky working with edinburgh weather

--- makesymlink.sh
+++ makesymlink.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim zshrc oh-my-zsh private scrotwm.conf Xresources aliases bash_profile tmux.conf conkyrc latexmkrc"    # list of files/folders to symlink in homedir
+files="bashrc vimrc vim zshrc oh-my-zsh private scrotwm.conf Xresources aliases bash_profile tmux.conf conkyrc latexmkrc conky-google-now"    # list of files/folders to symlink in homedir
 
 ##########
 
